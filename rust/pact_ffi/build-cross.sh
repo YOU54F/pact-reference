0c58f0776474e9002443162316a43164034721fb
Yousaf Nabi <you@saf.dev>
2023-05-01 19:54:35 +0100

    ci: test 1586/i686-unknown-linux-musl without target-feature=-crt-static

--- rust/pact_ffi/build-cross.sh
+++ rust/pact_ffi/build-cross.sh
@@ -130,7 +130,10 @@ for target in "${targets[@]}"; do
   if [[ $target == *"musl"* ]]; then
     echo "building for musl $target"
     # https://github.com/rust-lang/cargo/issues/7154
-    RUSTFLAGS="-C target-feature=-crt-static" cross build --target "${target}" --release
+    ## try and see if i686-unknown-linux-musl & i586-unknown-linux-musl will build without crt-static
+    ## this works for other musl targets
+    cross build --target "${target}" --release
+    # RUSTFLAGS="-C target-feature=-crt-static" cross build --target "${target}" --release
   elif [[ ($CI == "true" && $(uname -s) != "Linux") || $CIRRUS_CI = "true" ]]; then
     # no docker on github actions macos / windows
     # no docker in docker in cirrus
