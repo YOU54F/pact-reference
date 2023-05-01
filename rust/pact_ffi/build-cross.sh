72176514b94eecb8e35ec26980a2eee57160887c
Yousaf Nabi <you@saf.dev>
2023-05-01 15:38:37 +0100

    ci: use cargo incl musl tools

--- rust/pact_ffi/build-cross.sh
+++ rust/pact_ffi/build-cross.sh
@@ -130,7 +130,7 @@ for target in "${targets[@]}"; do
   if [[ $target == *"musl"* ]]; then
     echo "building for musl $target"
     # https://github.com/rust-lang/cargo/issues/7154
-    RUSTFLAGS="-C target-feature=-crt-static" cross build --target "${target}" --release
+    RUSTFLAGS="-C target-feature=-crt-static" cargo build --target "${target}" --release
   elif [[ (($target == *"windows"* || $target == *"apple"*)  && $CI == "true") || $CIRRUS_CI = "true" ]]; then
     # no docker on github actions macos / windows
     echo "building for $target with cargo"
