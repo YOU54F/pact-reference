0837af3d4474bfcff55575f5ebad663495e6a74f
Yousaf Nabi <yousaf@pactflow.io>
2024-05-20 21:23:56 +0100

    chore: verifier cli use cross

--- rust/pact_verifier_cli/release-linux.sh
+++ rust/pact_verifier_cli/release-linux.sh
@@ -8,18 +8,18 @@ RUST_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
 source "$RUST_DIR/scripts/gzip-and-sum.sh"
 ARTIFACTS_DIR=${ARTIFACTS_DIR:-"$RUST_DIR/release_artifacts"}
 mkdir -p "$ARTIFACTS_DIR"
-CARGO_TARGET_DIR=${CARO_TARGET_DIR:-"$RUST_DIR/target"}
+TARGET_DIR=${CARO_TARGET_DIR:-"$RUST_DIR/target"}
 
 # All flags passed to this script are passed to cargo.
 cargo_flags=( "$@" )
 
 build_x86_64() {
-    sudo apt-get install -y musl-tools
-    cargo build --target=x86_64-unknown-linux-musl "${cargo_flags[@]}"
+    # sudo apt-get install -y musl-tools
+    cross build --target=x86_64-unknown-linux-musl "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         gzip_and_sum \
-            "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/release/pact_verifier_cli" \
+            "$TARGET_DIR/x86_64-unknown-linux-musl/release/pact_verifier_cli" \
             "$ARTIFACTS_DIR/pact_verifier_cli-linux-x86_64.gz"
     fi
 }
@@ -29,14 +29,14 @@ install_cross() {
 }
 
 build_aarch64() {
-    install_cross
+    # install_cross
     cross build --target aarch64-unknown-linux-musl "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
-        gzip_and_sum "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/release/pact_verifier_cli" \
+        gzip_and_sum "$TARGET_DIR/aarch64-unknown-linux-musl/release/pact_verifier_cli" \
         "$ARTIFACTS_DIR/pact_verifier_cli-linux-aarch64.gz"
     fi
 }
-
+install_cross
 build_x86_64
 build_aarch64
