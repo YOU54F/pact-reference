68587681c155cd442ce933386f7cf5d6f98d83f4
Yousaf Nabi <yousaf@pactflow.io>
2024-05-20 21:39:51 +0100

    chore: set target-dir per target

--- rust/pact_ffi/release-linux.sh
+++ rust/pact_ffi/release-linux.sh
@@ -8,7 +8,7 @@ RUST_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
 source "$RUST_DIR/scripts/gzip-and-sum.sh"
 ARTIFACTS_DIR=${ARTIFACTS_DIR:-"$RUST_DIR/release_artifacts"}
 mkdir -p "$ARTIFACTS_DIR"
-export TARGET_DIR=${CARO_TARGET_DIR:-"$RUST_DIR/target"}
+export CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$RUST_DIR/target"}
 
 # All flags passed to this script are passed to cargo.
 cargo_flags=( "$@" )
@@ -17,14 +17,14 @@ cargo_flags=( "$@" )
 build_x86_64_gnu() {
     install_cross
     # cargo clean
-    cross build --target x86_64-unknown-linux-gnu "${cargo_flags[@]}"
+    cross build --target x86_64-unknown-linux-gnu --target-dir "$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu" "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         gzip_and_sum \
-            "$TARGET_DIR/x86_64-unknown-linux-gnu/release/libpact_ffi.a" \
+            "$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu/release/libpact_ffi.a" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64.a.gz"
         gzip_and_sum \
-            "$TARGET_DIR/x86_64-unknown-linux-gnu/release/libpact_ffi.so" \
+            "$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu/release/libpact_ffi.so" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64.so.gz"
     fi
 }
@@ -32,7 +32,7 @@ build_x86_64_gnu() {
 build_x86_64_musl() {
     # sudo apt-get install -y musl-tools
     # cargo clean
-    cross build --target x86_64-unknown-linux-musl "${cargo_flags[@]}"
+    cross build --target x86_64-unknown-linux-musl --target-dir "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl" "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         BUILD_SCRIPT=$(cat <<EOM
@@ -49,15 +49,15 @@ EOM
         docker run \
             --platform=linux/amd64 \
             --rm \
-            -v "$TARGET_DIR/x86_64-unknown-linux-musl/release:/scratch" \
+            -v "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/x86_64-unknown-linux-musl/release:/scratch" \
             alpine \
             /bin/sh -c "$BUILD_SCRIPT"
 
         gzip_and_sum \
-            "$TARGET_DIR/x86_64-unknown-linux-musl/release/libpact_ffi.a" \
+            "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/x86_64-unknown-linux-musl/release/libpact_ffi.a" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64-musl.a.gz"
         gzip_and_sum \
-            "$TARGET_DIR/x86_64-unknown-linux-musl/release/libpact_ffi.so" \
+            "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/x86_64-unknown-linux-musl/release/libpact_ffi.so" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64-musl.so.gz"
     fi
 }
@@ -69,14 +69,14 @@ install_cross() {
 build_aarch64_gnu() {
     install_cross
     # cargo clean
-    cross build --target aarch64-unknown-linux-gnu "${cargo_flags[@]}"
+    cross build --target aarch64-unknown-linux-gnu --target-dir "$CARGO_TARGET_DIR/aarch64-unknown-linux-gnu" "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         gzip_and_sum \
-            "$TARGET_DIR/aarch64-unknown-linux-gnu/release/libpact_ffi.a" \
+            "$CARGO_TARGET_DIR/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/release/libpact_ffi.a" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64.a.gz"
         gzip_and_sum \
-            "$TARGET_DIR/aarch64-unknown-linux-gnu/release/libpact_ffi.so" \
+            "$CARGO_TARGET_DIR/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/release/libpact_ffi.so" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64.so.gz"
     fi
 }
@@ -84,7 +84,7 @@ build_aarch64_gnu() {
 build_aarch64_musl() {
     install_cross
     # cargo clean
-    cross build --target aarch64-unknown-linux-musl "${cargo_flags[@]}"
+    cross build --target aarch64-unknown-linux-musl --target-dir "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl" "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         BUILD_SCRIPT=$(cat <<EOM
@@ -101,15 +101,15 @@ EOM
         docker run \
             --platform=linux/arm64 \
             --rm \
-            -v "$TARGET_DIR/aarch64-unknown-linux-musl/release:/scratch" \
+            -v "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/aarch64-unknown-linux-musl/release:/scratch" \
             alpine \
             /bin/sh -c "$BUILD_SCRIPT"
 
         gzip_and_sum \
-            "$TARGET_DIR/aarch64-unknown-linux-musl/release/libpact_ffi.a" \
+            "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/aarch64-unknown-linux-musl/release/libpact_ffi.a" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64-musl.a.gz"
         gzip_and_sum \
-            "$TARGET_DIR/aarch64-unknown-linux-musl/release/libpact_ffi.so" \
+            "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/release/libpact_ffi.so" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64-musl.so.gz"
     fi
 }
--- rust/pact_verifier_cli/release-linux.sh
+++ rust/pact_verifier_cli/release-linux.sh
@@ -8,18 +8,18 @@ RUST_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
 source "$RUST_DIR/scripts/gzip-and-sum.sh"
 ARTIFACTS_DIR=${ARTIFACTS_DIR:-"$RUST_DIR/release_artifacts"}
 mkdir -p "$ARTIFACTS_DIR"
-TARGET_DIR=${CARO_TARGET_DIR:-"$RUST_DIR/target"}
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$RUST_DIR/target"}
 
 # All flags passed to this script are passed to cargo.
 cargo_flags=( "$@" )
 
 build_x86_64() {
     # sudo apt-get install -y musl-tools
-    cross build --target=x86_64-unknown-linux-musl "${cargo_flags[@]}"
+    cross build --target=x86_64-unknown-linux-musl --target-dir "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl" "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         gzip_and_sum \
-            "$TARGET_DIR/x86_64-unknown-linux-musl/release/pact_verifier_cli" \
+            "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/x86_64-unknown-linux-musl/release/pact_verifier_cli" \
             "$ARTIFACTS_DIR/pact_verifier_cli-linux-x86_64.gz"
     fi
 }
@@ -30,10 +30,10 @@ install_cross() {
 
 build_aarch64() {
     # install_cross
-    cross build --target aarch64-unknown-linux-musl "${cargo_flags[@]}"
+    cross build --target aarch64-unknown-linux-musl --target-dir "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl" "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
-        gzip_and_sum "$TARGET_DIR/aarch64-unknown-linux-musl/release/pact_verifier_cli" \
+        gzip_and_sum "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/aarch64-unknown-linux-musl/release/pact_verifier_cli" \
         "$ARTIFACTS_DIR/pact_verifier_cli-linux-aarch64.gz"
     fi
 }
