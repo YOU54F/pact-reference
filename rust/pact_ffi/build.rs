f461d2a775842224cb3095c3ef599794f67895ac
Yousaf Nabi <yousaf@pactflow.io>
2024-05-29 19:01:02 +0100

    chore: test slim bins with linux ar static relocation symbols retained

--- rust/pact_ffi/build.rs
+++ rust/pact_ffi/build.rs
@@ -1,10 +1,15 @@
+use std::env;
 use os_info::Type;
 
 fn main() {
+  let target = std::env::var("TARGET").unwrap();
+  if target.contains("apple") {
     let info = os_info::get();
-    if info.os_type() == Type::Macos {
+    // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
+    if info.os_type() == Type::Macos && env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "macos" {
       // Remove hardcoded path to avoid need to use install_name_tool.
       // Drop file into a well-known path such as /usr/local/lib and it can be automatically discovered
       println!("cargo:rustc-cdylib-link-arg=-Wl,-install_name,libpact_ffi.dylib");
     }
+  }
 }
--- rust/pact_ffi/release-linux.sh
+++ rust/pact_ffi/release-linux.sh
@@ -8,104 +8,135 @@ RUST_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
 source "$RUST_DIR/scripts/gzip-and-sum.sh"
 ARTIFACTS_DIR=${ARTIFACTS_DIR:-"$RUST_DIR/release_artifacts"}
 mkdir -p "$ARTIFACTS_DIR"
-export CARGO_TARGET_DIR=${CARO_TARGET_DIR:-"$RUST_DIR/target"}
+export TARGET_DIR=${TARGET_DIR:-"$RUST_DIR/target"}
 
 # All flags passed to this script are passed to cargo.
 cargo_flags=( "$@" )
 
 # Build the x86_64 GNU linux release
 build_x86_64_gnu() {
+    # cargo clean
     install_cross
-    cargo clean
     cross build --target x86_64-unknown-linux-gnu "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
-        gzip_and_sum \
-            "$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/release/libpact_ffi.a" \
-            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64.a.gz"
-        gzip_and_sum \
-            "$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/release/libpact_ffi.so" \
-            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64.so.gz"
-    fi
-}
-
-build_x86_64_musl() {
-    sudo apt-get install -y musl-tools
-    cargo clean
-    cargo build --target x86_64-unknown-linux-musl "${cargo_flags[@]}"
-
-    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         BUILD_SCRIPT=$(cat <<EOM
-apk add --no-cache musl-dev gcc && \
 cd /scratch && \
-ar -x libpact_ffi.a && \
-gcc -shared *.o -o libpact_ffi.so && \
-rm -f *.o
+apt-get update && apt-get install -y gcc && \
+strip --strip-unneeded libpact_ffi.a && \
+strip libpact_ffi.so
 EOM
         )
 
         docker run \
             --platform=linux/amd64 \
             --rm \
-            -v "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/release:/scratch" \
-            alpine \
+            -v "$TARGET_DIRx86_64-unknown-linux-gnu/release:/scratch" \
+            debian \
             /bin/sh -c "$BUILD_SCRIPT"
 
         gzip_and_sum \
-            "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/release/libpact_ffi.a" \
-            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64-musl.a.gz"
+            "$TARGET_DIRx86_64-unknown-linux-gnu/release/libpact_ffi.a" \
+            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64.a.gz"
         gzip_and_sum \
-            "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/release/libpact_ffi.so" \
-            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64-musl.so.gz"
+            "$TARGET_DIR/x86_64-unknown-linux-gnu/release/libpact_ffi.so" \
+            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64.so.gz"
     fi
 }
 
-install_cross() {
-    cargo install cross@0.2.5
-}
-
-build_aarch64_gnu() {
+build_x86_64_musl() {
+    # sudo apt-get install -y musl-tools
+    # cargo clean
     install_cross
-    cargo clean
-    cross build --target aarch64-unknown-linux-gnu "${cargo_flags[@]}"
-
-    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
-        gzip_and_sum \
-            "$CARGO_TARGET_DIR/aarch64-unknown-linux-gnu/release/libpact_ffi.a" \
-            "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64.a.gz"
-        gzip_and_sum \
-            "$CARGO_TARGET_DIR/aarch64-unknown-linux-gnu/release/libpact_ffi.so" \
-            "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64.so.gz"
-    fi
-}
-
-build_aarch64_musl() {
-    install_cross
-    cargo clean
-    cross build --target aarch64-unknown-linux-musl "${cargo_flags[@]}"
+    RUSTFLAGS="-C target-feature=-crt-static" cross build --target x86_64-unknown-linux-musl "${cargo_flags[@]}"
 
     if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
         BUILD_SCRIPT=$(cat <<EOM
 apk add --no-cache musl-dev gcc && \
 cd /scratch && \
-ar -x libpact_ffi.a && \
-gcc -shared *.o -o libpact_ffi.so && \
-rm -f *.o
+strip --strip-unneeded libpact_ffi.a && \
+strip libpact_ffi.so
+EOM
+        )
+
+        docker run \
+            --platform=linux/amd64 \
+            --rm \
+            -v "$TARGET_DIR/x86_64-unknown-linux-musl/release:/scratch" \
+            alpine \
+            /bin/sh -c "$BUILD_SCRIPT"
+
+        gzip_and_sum \
+            "$TARGET_DIR/x86_64-unknown-linux-musl/release/libpact_ffi.a" \
+            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64-musl.a.gz"
+        gzip_and_sum \
+            "$TARGET_DIR/x86_64-unknown-linux-musl/release/libpact_ffi.so" \
+            "$ARTIFACTS_DIR/libpact_ffi-linux-x86_64-musl.so.gz"
+    fi
+}
+
+install_cross() {
+    cargo install cross@0.2.5 --force
+}
+install_cross_latest() {
+    cargo install cross --force
+}
+
+build_aarch64_gnu() {
+    install_cross
+    # cargo clean
+    cross build --target aarch64-unknown-linux-gnu "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        BUILD_SCRIPT=$(cat <<EOM
+cd /scratch && \
+apt-get update && apt-get install -y gcc && \
+strip --strip-unneeded libpact_ffi.a && \
+strip libpact_ffi.so
 EOM
         )
 
         docker run \
             --platform=linux/arm64 \
             --rm \
-            -v "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/release:/scratch" \
+            -v "$TARGET_DIR/aarch64-unknown-linux-gnu/release:/scratch" \
+            debian \
+            /bin/sh -c "$BUILD_SCRIPT"
+        gzip_and_sum \
+            "$TARGET_DIR/aarch64-unknown-linux-gnu/release/libpact_ffi.a" \
+            "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64.a.gz"
+        gzip_and_sum \
+            "$TARGET_DIR/aarch64-unknown-linux-gnu/release/libpact_ffi.so" \
+            "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64.so.gz"
+    fi
+}
+
+build_aarch64_musl() {
+    # cargo clean
+    install_cross_latest
+    RUSTFLAGS="-C target-feature=-crt-static" cross build --target aarch64-unknown-linux-musl "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        BUILD_SCRIPT=$(cat <<EOM
+apk add --no-cache musl-dev gcc && \
+cd /scratch && \
+strip --strip-unneeded libpact_ffi.a && \
+strip libpact_ffi.so
+EOM
+        )
+
+        docker run \
+            --platform=linux/arm64 \
+            --rm \
+            -v "$TARGET_DIR/aarch64-unknown-linux-musl/release:/scratch" \
             alpine \
             /bin/sh -c "$BUILD_SCRIPT"
 
         gzip_and_sum \
-            "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/release/libpact_ffi.a" \
+            "$TARGET_DIR/aarch64-unknown-linux-musl/release/libpact_ffi.a" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64-musl.a.gz"
         gzip_and_sum \
-            "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/release/libpact_ffi.so" \
+            "$TARGET_DIR/aarch64-unknown-linux-musl/release/libpact_ffi.so" \
             "$ARTIFACTS_DIR/libpact_ffi-linux-aarch64-musl.so.gz"
     fi
 }
